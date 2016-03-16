609d3e654831470e4fb5620ccee3685355970676
Nicklas <welle93@gmail.com>
2016-03-16 11:04:43 +0100

    added bootstrap theme.. and cleaned it

---
+++ sb-admin-2.Web1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sb_admin_2.Web1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+    }
+}
