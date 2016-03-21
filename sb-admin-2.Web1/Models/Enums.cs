3f210feb1abb6cfcefcf2c575ea51a89d0776a08
SplintDK <splint@splintnet.dk>
2016-03-21 11:12:18 +0100

    Model classes

---
+++ sb-admin-2.Web1/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace sb_admin_2.Web1.Models
+{
+    public enum TaskStatus
+    {
+        //Done
+        Done = 0, 
+        //Assigned but not started
+        Assigned = 1, 
+        //In progress
+        InProgress = 2
+
+    }
+}
