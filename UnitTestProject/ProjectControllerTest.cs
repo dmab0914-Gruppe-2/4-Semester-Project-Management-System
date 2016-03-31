76bcfd528c30c11f474d7396458fd4c72b0fd686
SplintDK <splint@splintnet.dk>
2016-03-31 12:16:25 +0200

    Lots of unit testing...

---
+++ UnitTestProject/ProjectControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ProjectControllerTest
+    {
+        [TestMethod]
+        public void CreateProject()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetProject()
+        {
+            
+        }
+
+        [TestMethod]
+        public void RemoveProject()
+        {
+            
+        }
+
+        [TestMethod]
+        public void AddTaskToMethod()
+        {
+            
+        }
+    
+    }
+}
