cb9ae10ea0c5d80b36427625cc7b27fee61f7492
Jacob Pedersen <jacob@midtpost.dk>
2016-04-01 09:17:05 +0200

    Test application for Data Access

---
+++ DataAccessConsoleTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.DataAccess;
+using Logic.Models;
+
+namespace DataAccessConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here we go!");
+            TestGetProject();
+            Console.ReadLine();
+        }
+
+        private static void TestGetProject()
+        {
+            Project project = DBProject.GetProject(1);
+            Console.WriteLine("##Project 1##");
+            Console.WriteLine("Project ID: " + project.Id);
+            Console.WriteLine("Project Title: " + project.Name);
+            Console.WriteLine("Project Description: " + project.Description);
+        }
+    }
+}
