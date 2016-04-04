9f94eaf606aac35dbf3fd33fe62c59f37f34c819
SplintDK <splint@splintnet.dk>
2016-04-04 10:54:58 +0200

    Various updates.. cant remember, made a lot on friday

---
+++ Logic/Controllers/Utility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Models;
+
+namespace Logic.Controllers
+{
+    public class Utility
+    {
+        public object Sanitizer(object input)
+        {
+            
+            if (input is Project)
+            {
+                Project project = new Project();
+                input = project;
+                project.Description = Sanitizer(project.Description);
+                project.Title = Sanitizer(project.Title);
+                project.LeaderUser.Email = Sanitizer(project.LeaderUser.Email);
+                project.LeaderUser.Username = Sanitizer(project.LeaderUser.Username);
+                return project;
+
+
+            }
+            if (input is Models.Task)
+            {
+                Models.Task task = new Models.Task();
+                
+
+            }
+
+
+
+            //throw new NotImplementedException("Didn't finish this function yet.");
+            return null;
+        }
+
+        private string Sanitizer(string input)
+        {
+            if (input.Contains("'"))
+            {
+                string output = input.Replace("'", "''");
+                return output;
+            }
+            return input;
+        }
+    }
+}
