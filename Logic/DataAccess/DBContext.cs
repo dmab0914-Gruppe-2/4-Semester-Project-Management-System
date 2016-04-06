83915264641889b88c89ad0df21771bfa4bf9cf1
Nicklas <welle93@gmail.com>
2016-04-06 10:45:01 +0200

    forgot to commit

--- Logic/DataAccess/DBContext.cs
+++ Logic/DataAccess/DBContext.cs
@@ -14,8 +14,7 @@ namespace Logic.DataAccess
     public sealed class DbContext : DataContext
     {
         private const string ServerAdress = "localhost"; //Host adress. example localhost or ip or domain name.
-        private const string ServerSubUrlName = "SQLEXPRESS"; //Fill if rdbms is located by url name. Example MS Sql server is usually "SQLEXPRESS". Else let it be empty or null.
-        private const string Database = "PMS"; //The database name.
+        private const string ServerSubUrlName = "SQLEXPRESS"; //Fill if rdbms is located by url name. Example MS Sql server is usually "SQLEXPRESS". Else let it be empty or null. private const string Database = "PMS"; //The database name.
         private const string Username = "sa"; //DB username if not using integrated security.
         private const string Password = ""; //DB password if not using integrated security.
         private const bool UseIntegratedSecurity = true; //use integrated security instead of username and password.
@@ -116,6 +115,7 @@ namespace Logic.DataAccess
 
         //public Table<User> Users;
         public Table<Project> Projects;
+        public Table<Task> Tasks;
         //public Table<ProjectUsers> ProjectUsers;
         //public Table<ProjectFiles> ProjectFiles;
         //public Table<ChatMessage> ChatMessages;
