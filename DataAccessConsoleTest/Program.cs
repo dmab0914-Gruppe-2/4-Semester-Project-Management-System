2cd2e336d25a24e5e31dd70f7be0d0f5c465ba6b
Jacob Pedersen <jacob@midtpost.dk>
2016-04-11 10:21:51 +0200

    more test stuff for db project

--- DataAccessConsoleTest/Program.cs
+++ DataAccessConsoleTest/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Logic.DataAccess;
 using Logic.Models;
+using Task = Logic.Models.Task;
 
 namespace DataAccessConsoleTest
 {
@@ -14,11 +15,20 @@ namespace DataAccessConsoleTest
         static void Main(string[] args)
         {
             Console.WriteLine("Here we go!");
-            TestGetProject();
+            TestProject();
+            //TestTask();
             Console.ReadLine();
         }
 
-        private static void TestGetProject()
+        private static void TestTask()
+        {
+            DbTask dbTask = new DbTask();
+            Console.WriteLine("##Testing retrieval of getting a Task.");
+            PrintTaskInformation(dbTask.GetTask(1));
+            Console.WriteLine("##DONE");
+        }
+
+        private static void TestProject()
         {
             DbProject dbProject = new DbProject();
             Console.WriteLine("##Testing retrieval of 1'st project: Project 1##");
@@ -30,15 +40,59 @@ namespace DataAccessConsoleTest
                 PrintProjectInformation(project);
             }
             //sConsole.WriteLine(DateTime.UtcNow);
-
+            Console.WriteLine("##Adding project to DB##");
+            Project addingproject = new Project();
+            addingproject.Title = "The project addition";
+            addingproject.CreatedDate = DateTime.UtcNow;
+            addingproject.Description = "This project got added from the application";
+            addingproject.Done = false;
+            addingproject.LastChange = DateTime.UtcNow;
+            dbProject.AddProject(addingproject);
+            Console.WriteLine("##Project added to db##\nLets test retrieving it.");
+            Console.WriteLine("Retrieving added proejct..");
+            addingproject = dbProject.GetProject(addingproject.Title).FirstOrDefault();
+            if (addingproject.Id != null)
+            {
+                Console.WriteLine("Project id is: " + addingproject.Id.Value);
+                PrintProjectInformation(dbProject.GetProject(addingproject.Id.Value));
+                Console.WriteLine("##Removing previously added proejct.. Sucess?:" + dbProject.RemoveProject(addingproject.Id.Value));
+            }
+            else
+            {
+                Console.WriteLine("Project id is null..");
+            }
+            Console.WriteLine("##DONE##");
+            
         }
 
         private static void PrintProjectInformation(Project project)
         {
-            Console.WriteLine("##Project Information##");
-            Console.WriteLine("Project ID: " + project.Id);
-            Console.WriteLine("Project Title: " + project.Title);
-            Console.WriteLine("Project Description: " + project.Description);
+            if (project == null)
+            {
+                Console.WriteLine("That project didn't exist or something else happened..");
+            }
+            else
+            {
+                Console.WriteLine("##Project Information##");
+                Console.WriteLine("Project ID: " + project.Id);
+                Console.WriteLine("Project Title: " + project.Title);
+                Console.WriteLine("Project Description: " + project.Description);
+            }
+
+        }
+
+        private static void PrintTaskInformation(Task task)
+        {
+            if (task == null)
+            {
+                Console.WriteLine("That task didn't exist or something else happened..");
+            }
+            else
+            {
+                Console.WriteLine("##Task Informaltion##");
+                Console.WriteLine("Task Id: " + task.Id);
+                Console.WriteLine("Task Title" + task.Title);
+            }
         }
     }
 }
