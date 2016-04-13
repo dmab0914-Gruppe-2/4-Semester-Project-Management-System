bc329a52cb368cc9af3dd9b2e103919cae7696f6
SplintDK <splint@splintnet.dk>
2016-04-13 10:42:57 +0200

    Checking string length in task controller

--- Logic/Controllers/TaskController.cs
+++ Logic/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Logic.Models;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,74 +26,89 @@ namespace Logic.Controllers
         public ReturnValue CreateTask(string title, string description, Priority priority, TaskStatus ts,
             int projectId, DateTime duedate)
         {
-            Models.Task task = new Models.Task
+            if (utility.StringLength50(title))
             {
-                Title = title,
-                Description = description,
-                Priority = priority,
-                ProjectId = projectId,
-                DueDate = duedate,
-                Created = DateTime.UtcNow,
-                LastEdited = DateTime.UtcNow,
-                Status = ts
-            };
-            Models.Task returnTask  = (Models.Task) utility.Sanitizer(task);
-            return AddTask(returnTask);
-
+                Models.Task task = new Models.Task
+                {
+                    Title = title,
+                    Description = description,
+                    Priority = priority,
+                    ProjectId = projectId,
+                    DueDate = duedate,
+                    Created = DateTime.UtcNow,
+                    LastEdited = DateTime.UtcNow,
+                    Status = ts
+                };
+                Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
+                return AddTask(returnTask);
+            }
+            return ReturnValue.Fail;
 
         }
 
         public ReturnValue CreateTask(string title, string description, Priority priority, TaskStatus ts, int projectId)
         {
-            Models.Task task = new Models.Task
+            if (utility.StringLength50(title))
             {
-                Title = title,
-                Description = description,
-                Status = ts,
-                Created = DateTime.UtcNow,
-                Priority = priority,
-                ProjectId = projectId,
-                DueDate = DateTime.MaxValue,
-                LastEdited = DateTime.UtcNow
+                Models.Task task = new Models.Task
+                {
+                    Title = title,
+                    Description = description,
+                    Status = ts,
+                    Created = DateTime.UtcNow,
+                    Priority = priority,
+                    ProjectId = projectId,
+                    DueDate = DateTime.MaxValue,
+                    LastEdited = DateTime.UtcNow
 
-            };
-            Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
-            return AddTask(returnTask);
+                };
+                Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
+                return AddTask(returnTask);
+            }
+            return ReturnValue.Fail;
         }
 
         public ReturnValue CreateTask(string title, string description, Priority priority, int projectId)
         {
-            Models.Task task = new Models.Task
+            if (utility.StringLength50(title))
             {
-                Title = title,
-                Description = description,
-                Status = TaskStatus.Unassigned,
-                Created = DateTime.UtcNow,
-                DueDate = DateTime.MaxValue,
-                LastEdited = DateTime.UtcNow,
-                Priority = priority,
-                ProjectId = projectId,
+                Models.Task task = new Models.Task
+                {
+                    Title = title,
+                    Description = description,
+                    Status = TaskStatus.Unassigned,
+                    Created = DateTime.UtcNow,
+                    DueDate = DateTime.MaxValue,
+                    LastEdited = DateTime.UtcNow,
+                    Priority = priority,
+                    ProjectId = projectId,
 
-            };
-            Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
-            return AddTask(returnTask);
+                };
+                Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
+                return AddTask(returnTask);
+            }
+            return ReturnValue.Fail;
         }
 
         public ReturnValue CreateTask(string title, string description, Priority priority, int projectId, DateTime duedate)
         {
-            Models.Task task = new Task
+            if (utility.StringLength50(title))
             {
-                Title = title,
-                Description = description,
-                Priority = priority,
-                ProjectId = projectId,
-                DueDate = duedate,
-                LastEdited = DateTime.UtcNow,
-                Created = DateTime.UtcNow,
-                Status = TaskStatus.Unassigned
-            };
-            Models.Task returnTask = (Models.Task) utility.Sanitizer(task);
-            return AddTask(returnTask);
+                Models.Task task = new Task
+                {
+                    Title = title,
+                    Description = description,
+                    Priority = priority,
+                    ProjectId = projectId,
+                    DueDate = duedate,
+                    LastEdited = DateTime.UtcNow,
+                    Created = DateTime.UtcNow,
+                    Status = TaskStatus.Unassigned
+                };
+                Models.Task returnTask = (Models.Task)utility.Sanitizer(task);
+                return AddTask(returnTask);
+            }
+            return ReturnValue.Fail;
         }
 
         public Models.Task[] GetTask(string title)
@@ -113,7 +129,7 @@ namespace Logic.Controllers
         public ReturnValue RemoveTask(int id)
         {
             DbTask.RemoveTask(id);
-            if(DbTask.GetTask(id) == null)
+            if (DbTask.GetTask(id) == null)
                 return ReturnValue.Success;
             return ReturnValue.Fail;
         }
@@ -129,7 +145,7 @@ namespace Logic.Controllers
                 default:
                     return ReturnValue.UnknownFail;
             }
-            
+
             //switch (container.AddTask(task))
             //{
             //    //Success add
@@ -146,17 +162,22 @@ namespace Logic.Controllers
 
         public ReturnValue UpdateTask(Models.Task task)
         {
-            Task returnTask= (Models.Task)utility.Sanitizer(task);
-            returnTask.LastEdited = DateTime.UtcNow;
-            bool success = DbTask.UpdateTast(returnTask);
-            if(!success)
+            if (task.Id != null && utility.StringLength50(task.Title))
+            {
+                Task returnTask = (Models.Task)utility.Sanitizer(task);
+                returnTask.LastEdited = DateTime.UtcNow;
+                bool success = DbTask.UpdateTast(returnTask);
+                if (!success)
+                    return ReturnValue.Fail;
+                Debug.Assert(task.Id != null, "task.Id != null");
+                task = DbTask.GetTask((int)task.Id.Value);
+                if (task.Title.Equals(returnTask.Title) &&
+                    task.Description.Equals(returnTask.Description) &&
+                    task.Status.Equals(returnTask.Status) &&
+                    task.Priority.Equals(returnTask.Priority))
+                    return ReturnValue.Success;
                 return ReturnValue.Fail;
-            task = DbTask.GetTask((int) task.Id.Value);
-            if(task.Title.Equals(returnTask.Title) &&
-                task.Description.Equals(returnTask.Description) &&
-                task.Status.Equals(returnTask.Status) && 
-                task.Priority.Equals(returnTask.Priority))
-                return ReturnValue.Success;
+            }
             return ReturnValue.Fail;
         }
 
